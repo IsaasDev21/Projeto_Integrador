22ac648759e3ae6bfe889e92c1b1a53f8c7583d9
IsaasDev21 <rangelisaac11@gmail.com>
2023-11-10 11:38:25 -0300

    first commit1

---
+++ js/login-script.js
@@ -0,0 +1,10 @@
+const form = document.querySelector("form") 
+const inpCpf = document.querySelector("inpCpf");
+const inpSenha = document.querySelector("inpSenha");
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    alert("LOGIN REALIZADO")
+
+})
